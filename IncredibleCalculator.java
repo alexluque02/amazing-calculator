681cfb126a5d3e5524b1faada2ff1f454bda7d79
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-01-24 09:48:27 +0100

    Añadimos la operación multiplicación

--- IncredibleCalculator.java
+++ IncredibleCalculator.java
@@ -25,7 +25,10 @@ public class IncredibleCalculator {
                     int resultado=n1-n2;
                      System.out.println("La resta de "+n1+" - "+n2+" es: "+resultado)
                     break;
-            case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
+            case 3: int n1=leerEnteroEntrada();
+                    int n2=leerEnteroEntrada();
+                    int resultado=n1*n2;
+                     System.out.println("La multiplicación de "+n1+" * "+n2+" es: "+resultado)
                     break;
             case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
                     break;
